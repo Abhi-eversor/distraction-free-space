5199cf28f37b5a18f9f4f2b08e894478ca2cf1a5
Abhishek Aryan <abhi.aryan021@gmail.com>
2026-01-11 11:34:48 +0530

    updated app.js

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
+
+const authRoutes = require("./routes/auth");
+const docRoutes = require("./routes/docs");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Backend is running");
-});
+app.use("/api/auth", authRoutes);
+app.use("/api/docs", docRoutes);
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => {
+    console.error("MongoDB connection error:", err);
+    process.exit(1);
+  });
 
 module.exports = app;
